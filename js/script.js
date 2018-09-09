feb35bbbab4ecb866e3684eb53a414f2c677df71
silverfly555 <40993262+silverfly555@users.noreply.github.com>
2018-09-09 23:17:23 +0300

    a

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
   var link = document.querySelector(".button-search");
+  var popup = document.querySelector("form.popup-reserve");
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();
     console.log("Клик по кнопке")
+    popup.classList.toggle("modal-show");
   });
-
-  var popup = document.querySelector(".popap-reserve");
